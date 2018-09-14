d9a735eff457ac33a0984a77d12e5fb5065f153d
Sihan-Son <sihan830@gmail.com>
2018-09-14 18:02:36 +0900

    [ADD] testing code

--- classification/testing.py
+++ classification/testing.py
@@ -5,17 +5,45 @@ import tensorflow as tf
 from PIL import Image
 import numpy as np
 import os
+import glob
 
 os.environ['TF_CPP+MIN_LOG_LEVEL'] = '3'
 
 image_width = 64
 image_height = 64
+# 지도 학습을 위한 라벨
+image_label = []
+# 리사이징 된 이미지
+All_image = []
+
+
 input_data = []
 
-img = Image.open("./data/a_set\dogdog.jpg")
-img = img.resize((image_width, image_height))
 
-input_data.append(np.float32(img))
+# img = Image.open("./data/a_set\dog.4001.jpg")
+# img = img.resize((image_width, image_height))
+#
+# input_data.append(np.float32(img))
+
+# 테스트용 데이터 읽어 오기
+img_path = os.getcwd()
+image_file_list = glob.glob(img_path + "\data\\a_set\*.jpg")
+
+# 이미지 리사이징 및 라벨링
+for img in image_file_list:
+    # 라벨링
+    if 'kitten' in img:
+        image_label.append(0)
+    else:
+        image_label.append(1)
+
+    image = Image.open(img)
+    image = image.resize((image_width, image_height))
+    All_image.append(np.float32(image))
+label = np.eye(2)[image_label]
+
+# 예측값
+
 
 # 학습 시킬 변수, input data
 X = tf.placeholder(tf.float32, [None, image_width, image_height, 3])
@@ -61,11 +89,26 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     save_path = "./checkpoint/animal.ckpt"
     saver.restore(sess, save_path)
-    predict = tf.argmax(model, 1)
-    predictions = sess.run(predict, feed_dict={X: input_data, keep_prob: 1.0})
-    print(predictions[0])
 
-    if predictions[0] == 0:
-        print("cat")
-    else:
-        print("dog")
+    predict = tf.argmax(model, 1)
+    # 실제값
+    original = tf.argmax(Y, 1)
+
+    # 일치 하는지 확인
+    is_correct = tf.equal(predict, original)
+    # 정확도
+    accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+
+    print(sess.run(predict, feed_dict={X: All_image, keep_prob: 1}))
+    print(sess.run(original, feed_dict={Y: label}))
+
+    print("ACC >> ", sess.run(accuracy, feed_dict={X: All_image, Y: label, keep_prob: 1}))
+
+    # predict = tf.argmax(model, 1)
+    # predictions = sess.run(predict, feed_dict={X: input_data, keep_prob: 1.0})
+    # print(predictions[0])
+    #
+    # if predictions[0] == 0:
+    #     print("cat")
+    # else:
+    #     print("dog")
