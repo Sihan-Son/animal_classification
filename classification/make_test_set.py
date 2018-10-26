232064750a1e8d82bf941239f409fb701bb6ad3d
Sihan-Son <sihan830@gmail.com>
2018-10-26 23:15:06 +0900

    [ADD] model test

--- classification/make_test_set.py
+++ classification/make_test_set.py
@@ -1,10 +1,11 @@
-import cv2  # working with, mainly resizing, images
-import numpy as np  # dealing with arrays
 import os  # dealing with directories
 from random import shuffle  # mixing up or currently ordered data that might lead our network astray in training.
+
+import cv2  # working with, mainly resizing, images
+import numpy as np  # dealing with arrays
 from tqdm import tqdm  # a nice pretty percentage bar for tasks. Thanks to viewer Daniel BA1/4hler for this suggestion
 
-TRAIN_DIR = 'E:\Project\Ai_Club\dataSets\\train'
+TEST_DIR = 'E:\Project\Ai_Club\dataSets\\train'
 TEST_DIR = 'E:\Project\Ai_Club\dataSets\\test'
 IMG_SIZE = 50
 LR = 1e-3
@@ -23,15 +24,27 @@ def label_img(img):
 
 def create_train_data():
     training_data = []
-    for img in tqdm(os.listdir(TRAIN_DIR)):
+    for img in tqdm(os.listdir(TEST_DIR)):
         label = label_img(img)
-        path = os.path.join(TRAIN_DIR, img)
+        path = os.path.join(TEST_DIR, img)
         img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
         img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
         training_data.append([np.array(img), np.array(label)])
     shuffle(training_data)
     np.save('./data/train_data.npy', training_data)
     return training_data
+
+def create_test_data():
+    test_data = []
+    for img in tqdm(os.listdir(TEST_DIR)):
+        label = label_img(img)
+        path = os.path.join(TEST_DIR, img)
+        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+        test_data.append([np.array(img), np.array(label)])
+    shuffle(test_data)
+    np.save('./data/test2_data.npy', test_data)
+    return test_data
 
 
 def process_test_data():
@@ -48,5 +61,6 @@ def process_test_data():
     return testing_data
 
 
-test_data = process_test_data()
-train_data = create_train_data()
+# test_data = process_test_data()
+# train_data = create_train_data()
+test_data2 = create_test_data()
