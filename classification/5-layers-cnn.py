8400e34e90224fb10911fc8e49c8cfcc307e8530
Sihan-Son <sihan830@gmail.com>
2018-10-26 23:38:49 +0900

    [ADD] 5-layers-cnn

--- classification/5-layers-cnn.py
+++ classification/5-layers-cnn.py
@@ -5,13 +5,18 @@ from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
 
+import tensorflow as tf
 
-IMG_SIZE = 50
-LR = 1e-6
-MODEL_NAME = 'dogsVScats-{}-{}.model'.format(LR, '2conv-basic')
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 train_data = np.load("./data/train_data.npy")
 test_data = np.load("./data/test2_data.npy")
+
+IMG_SIZE = 50
+LR = 1e-5
+MODEL_NAME = 'dogsVScats-{}-{}.model'.format(LR, '2conv-basic')
+
+tf.reset_default_graph()
 
 convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
 
@@ -19,6 +24,15 @@ convnet = conv_2d(convnet, 32, 5, activation='relu')
 convnet = max_pool_2d(convnet, 5)
 
 convnet = conv_2d(convnet, 64, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 128, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 64, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 32, 5, activation='relu')
 convnet = max_pool_2d(convnet, 5)
 
 convnet = fully_connected(convnet, 1024, activation='relu')
@@ -44,3 +58,34 @@ test_y = [i[1] for i in test]
 
 model.fit({'input': X}, {'targets': Y}, n_epoch=3, validation_set=({'input': test_x}, {'targets': test_y}),
           snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+model.save(".\\test\\" + MODEL_NAME)
+
+import matplotlib.pyplot as plt
+
+test_data = np.load('./data/test_data.npy')
+
+fig = plt.figure()
+
+for num, data in enumerate(test_data[:12]):
+    # cat: [1,0]
+    # dog: [0,1]
+
+    img_num = data[1]
+    img_data = data[0]
+
+    y = fig.add_subplot(3, 4, num + 1)
+    orig = img_data
+    data = img_data.reshape(IMG_SIZE, IMG_SIZE, 1)
+    # model_out = model.predict([data])[0]
+    model_out = model.predict([data])[0]
+
+    if np.argmax(model_out) == 1:
+        str_label = 'Dog'
+    else:
+        str_label = 'Cat'
+
+    y.imshow(orig, cmap='gray')
+    plt.title(str_label)
+    y.axes.get_xaxis().set_visible(False)
+    y.axes.get_yaxis().set_visible(False)
+plt.show()
