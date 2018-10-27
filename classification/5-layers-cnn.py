1c7c046c2d80679b61229d59d38e1b41370a24c5
Sihan-Son <sihan830@gmail.com>
2018-10-27 09:43:04 +0900

    [update] acc 77 달성!!

--- classification/5-layers-cnn.py
+++ classification/5-layers-cnn.py
@@ -12,11 +12,11 @@ import tensorflow as tf
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-train_data = np.load("./data/train_data.npy")
-test_data = np.load("./data/test2_data.npy")
+train_data = np.load("./data/train_data_25000.npy")
+test_data = np.load("./data/test_data_12500.npy")
 
 IMG_SIZE = 50
-LR = 1e-5
+LR = 1e-4
 MODEL_NAME = 'dogsVScats-{}-{}.model'.format(LR, '2conv-basic')
 
 tf.reset_default_graph()
@@ -50,8 +50,8 @@ if os.path.exists('{}.meta'.format(MODEL_NAME)):
     model.load(MODEL_NAME)
     print('model loaded!')
 
-train = train_data[:]
-test = test_data[:]
+train = train_data[:-500]
+test = train_data[-500:]
 
 X = np.array([i[0] for i in train]).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 Y = [i[1] for i in train]
@@ -59,36 +59,38 @@ Y = [i[1] for i in train]
 test_x = np.array([i[0] for i in test]).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 test_y = [i[1] for i in test]
 
-model.fit({'input': X}, {'targets': Y}, n_epoch=3, validation_set=({'input': test_x}, {'targets': test_y}),
+model.fit({'input': X}, {'targets': Y}, n_epoch=6, validation_set=({'input': test_x}, {'targets': test_y}),
           snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
 model.save(".\\test\\" + MODEL_NAME)
 
-import matplotlib.pyplot as plt
 
-test_data = np.load('./data/test_data.npy')
 
-fig = plt.figure()
-
-for num, data in enumerate(test_data[:12]):
-    # cat: [1,0]
-    # dog: [0,1]
-
-    img_num = data[1]
-    img_data = data[0]
-
-    y = fig.add_subplot(3, 4, num + 1)
-    orig = img_data
-    data = img_data.reshape(IMG_SIZE, IMG_SIZE, 1)
-    # model_out = model.predict([data])[0]
-    model_out = model.predict([data])[0]
-
-    if np.argmax(model_out) == 1:
-        str_label = 'Dog'
-    else:
-        str_label = 'Cat'
-
-    y.imshow(orig, cmap='gray')
-    plt.title(str_label)
-    y.axes.get_xaxis().set_visible(False)
-    y.axes.get_yaxis().set_visible(False)
-plt.show()
+# import matplotlib.pyplot as plt
+#
+# test_data = np.load('./data/test_data_12500.npy')
+#
+# fig = plt.figure()
+#
+# for num, data in enumerate(test_data[:12]):
+#     # cat: [1,0]
+#     # dog: [0,1]
+#
+#     img_num = data[1]
+#     img_data = data[0]
+#
+#     y = fig.add_subplot(3, 4, num + 1)
+#     orig = img_data
+#     data = img_data.reshape(IMG_SIZE, IMG_SIZE, 1)
+#     # model_out = model.predict([data])[0]
+#     model_out = model.predict([data])[0]
+#
+#     if np.argmax(model_out) == 1:
+#         str_label = 'Dog'
+#     else:
+#         str_label = 'Cat'
+#
+#     y.imshow(orig, cmap='gray')
+#     plt.title(str_label)
+#     y.axes.get_xaxis().set_visible(False)
+#     y.axes.get_yaxis().set_visible(False)
+# plt.show()
