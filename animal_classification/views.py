93474b09468ea4f9372664ed4307b274da37152b
Sihan-Son <sihan830@gmail.com>
2019-02-21 14:36:23 +0900

    TEST

--- animal_classification/views.py
+++ animal_classification/views.py
@@ -72,4 +72,5 @@ class ClassificationResultView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         print(context)
+        """hello this is test line"""
         return context
